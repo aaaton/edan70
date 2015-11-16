8c52575012c0c594917f44d925730d836b10dae5
Anton Sodergren <anton@monokulturen.se>
2015-11-16 10:56:18 +0100

    Created first files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Selma Lagerlöf</title>
+  </head>
+  <body>
+    <div class="header">
+      <h1>Selma Lagerlöf</h1>
+      <h2>Title of book</h2>
+    </div>
+    <div class="menu">
+
+    </div>
+    <div class="body">
+      Body of text
+    </div>
+  </body>
+</html>
