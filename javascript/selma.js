4b148029b50c54dfdd38893480100820f4c3f086
AAAton <anton@monokulturen.se>
2015-12-01 11:25:09 +0100

    update

--- javascript/selma.js
+++ javascript/selma.js
@@ -24,7 +24,7 @@ function toggleMenu() {
     $(".menu").animate({width:'100px'});
     $(".menu").addClass("hidden");
   } else {
-    $(".menu").animate({width:'300px'});
+    $(".menu").animate({width:'200px'});
     $(".menu").removeClass("hidden");
   }
   menuOpen = !menuOpen;
